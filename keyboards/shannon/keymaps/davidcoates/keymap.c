f923d7d143c37870c7d789a709c750e5faf3c269
David <davidcoates95@gmail.com>
2026-09-15 08:23:52 +1000

    shannon tweaks

--- keyboards/shannon/keymaps/davidcoates/keymap.c
+++ keyboards/shannon/keymaps/davidcoates/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_J,           KC_L,           KC_U,           KC_Y,           KC_Z,
     KC_A,           KC_R,           KC_S,           KC_T,           KC_G,           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,
     OSM(MOD_LSFT),  KC_X,           KC_C,           KC_D,           KC_V,           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         MO(_ARROW),
-                    KC_LEFT_CTRL,   SYM_CTRL,       KC_SPACE,                       KC_BSPC,        OSL(_NUMPAD),   KC_TAB,
-    KC_ESCAPE,                                                                                                                      KC_ENTER
+                    KC_LEFT_CTRL,   SYM_CTRL,       KC_SPACE,                                       KC_BSPC,        OSL(_NUMPAD),   KC_TAB,
+    KC_ESCAPE,                                                                                                                                      KC_ENTER
   ),
 
   // Symbol layer
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_QUES,        KC_QUOTE,       KC_SLASH,       KC_ASTR,        KC_GRAVE,       KC_PLUS,        KC_BSLS,        KC_COLN,        KC_MINUS,       KC_DQUO,
     KC_UNDS,        KC_AMPR,        KC_LCBR,        KC_RCBR,        KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_EQUAL,       KC_SCLN,
     KC_TILD,        KC_EXLM,        KC_PERC,        KC_DLR,         KC_PIPE,        KC_CIRC,        KC_HASH,        KC_LABK,        KC_RABK,        KC_AT,
-                    KC_TRNS,        KC_TRNS,        KC_NO,                          KC_DELETE,      KC_NO,          UM(EN_DASH),
-    KC_TRNS,                                                                                                                        KC_TRNS
+                    KC_TRNS,        KC_TRNS,        KC_NO,                                          KC_DELETE,      KC_NO,          UM(EN_DASH),
+    KC_TRNS,                                                                                                                                        KC_TRNS
   ),
 
   // Numpad layers
@@ -45,16 +45,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          MO(_FUNCTION),  KC_NO,          KC_NO,          KC_NO,          KC_7,           KC_8,           KC_9,           KC_NO,
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_4,           KC_5,           KC_6,           KC_0,
     KC_LEFT_SHIFT,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_1,           KC_2,           KC_3,           KC_DOT,
-                    KC_TRNS,        KC_NO,          KC_NO,                          KC_NO,          KC_TRNS,        KC_NO,
-    KC_TRNS,                                                                                                                        KC_TRNS
+                    KC_TRNS,        KC_NO,          KC_NO,                                          KC_NO,          KC_TRNS,        KC_NO,
+    KC_TRNS,                                                                                                                                        KC_TRNS
   ),
 
   [_FUNCTION] = LAYOUT(
     KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_F7,          KC_F8,          KC_F9,          KC_F10,
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_F4,          KC_F5,          KC_F6,          KC_F11,
     KC_CAPS_LOCK,   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F12,
-                    KC_NO,          KC_NO,          KC_NO,                          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,                                                                                                                          KC_NO
+                    KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,                                                                                                                                          KC_NO
   ),
 
   // Navigation layers
@@ -63,24 +63,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PAGE_UP,     KC_HOME,        KC_UP,          KC_END,         KC_PLUS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_PAGE_DOWN,   KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_MINUS,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_LEFT_SHIFT,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
-                    KC_TRNS,        KC_NO,          KC_NO,                          KC_NO,          MO(_WINDOW),    MO(_MOUSE),
-    KC_TRNS,                                                                                                                        KC_TRNS
+                    KC_TRNS,        KC_NO,          KC_NO,                                          KC_NO,          MO(_WINDOW),    MO(_MOUSE),
+    KC_TRNS,                                                                                                                                        KC_TRNS
   ),
 
   [_MOUSE] = LAYOUT(
     MS_WHLU,        MS_BTN2,        MS_UP,          MS_BTN1,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     MS_WHLD,        MS_LEFT,        MS_DOWN,        MS_RGHT,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                    KC_TRNS,        KC_NO,          KC_NO,                          KC_NO,          KC_NO,          KC_TRNS,
-    KC_TRNS,                                                                                                                        KC_TRNS
+                    KC_TRNS,        KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_TRNS,
+    KC_TRNS,                                                                                                                                        KC_TRNS
   ),
 
   [_WINDOW] = LAYOUT(
     KC_NO,          LCTL(KC_DOWN),  KC_TAB,         LCTL(KC_UP),    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_NO,          LCTL(KC_LEFT),  LSFT(KC_TAB),   LCTL(KC_RIGHT), KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                    KC_TRNS,        KC_NO,          KC_NO,                          KC_NO,          KC_TRNS,        KC_NO,
-    KC_TRNS,                                                                                                                        KC_TRNS
+                    KC_TRNS,        KC_NO,          KC_NO,                                          KC_NO,          KC_TRNS,        KC_NO,
+    KC_TRNS,                                                                                                                                        KC_TRNS
   ),
 
   // Control / misc layer
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_QWERTY),    KC_WIN_SEL,     KC_VOLU,        KC_MPLY,        QK_BOOT,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_SCR_SHOT,    KC_MPRV,        KC_VOLD,        KC_MNXT,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                    KC_NO,          KC_TRNS,        KC_NO,                          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,                                                                                                                          KC_NO
+                    KC_NO,          KC_TRNS,        KC_NO,                                          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,                                                                                                                                          KC_NO
   ),
 
   // Qwerty layer (for games / applications, not typing)
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_BASE),      KC_Q,           KC_W,           KC_E,           KC_R,           KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_NO,          KC_A,           KC_S,           KC_D,           KC_F,           KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                    KC_LEFT_CTRL,   KC_NO,          KC_SPACE,                       KC_NO,          KC_NO,          KC_NO,
-    KC_ESCAPE,                                                                                                                      KC_NO
+                    KC_LEFT_CTRL,   KC_NO,          KC_SPACE,                                       KC_NO,          KC_NO,          KC_NO,
+    KC_ESCAPE,                                                                                                                                      KC_NO
   ),
 
 };
